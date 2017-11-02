c16c2d6472b6fd83897deff17b776e34ef4dc58e
Colin Fallon <c.fallon@cern.ch>
2017-11-02 15:57:44 -0500

    Added new signal dict to batch

---
+++ batch/signals2.py
@@ -0,0 +1,63 @@
+flist = [
+    {
+        "mZprime": 1000,
+        "mDark": 20,
+        "rinv": 0.3,
+        "alpha": 0.2,
+    },
+    {
+        "mZprime": 2000,
+        "mDark": 20,
+        "rinv": 0.3,
+        "alpha": 0.2,
+    },
+    {
+        "mZprime": 4000,
+        "mDark": 20,
+        "rinv": 0.3,
+        "alpha": 0.2,
+    },
+
+    {
+        "mZprime": 3000,
+        "mDark": 1,
+        "rinv": 0.3,
+        "alpha": 0.2,
+    },
+    {
+        "mZprime": 3000,
+        "mDark": 50,
+        "rinv": 0.3,
+        "alpha": 0.2,
+    },
+    {
+        "mZprime": 3000,
+        "mDark": 100,
+        "rinv": 0.3,
+        "alpha": 0.2,
+    },
+    {
+        "mZprime": 3000,
+        "mDark": 20,
+        "rinv": 0.1,
+        "alpha": 0.2,
+    },
+    {
+        "mZprime": 3000,
+        "mDark": 20,
+        "rinv": 0.7,
+        "alpha": 0.2,
+    },
+    {
+        "mZprime": 3000,
+        "mDark": 20,
+        "rinv": 0.3,
+        "alpha": 0.5,
+    },
+    {
+        "mZprime": 3000,
+        "mDark": 20,
+        "rinv": 0.3,
+        "alpha": 1.0,
+    },
+]
