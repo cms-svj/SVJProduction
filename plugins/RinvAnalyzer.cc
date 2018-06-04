3afabb516638ad75bf39fdf3a66117a8f1203727
Kevin Pedro <kpedro88@gmail.com>
2018-06-04 09:59:48 -0500

    add binomial errors

--- plugins/RinvAnalyzer.cc
+++ plugins/RinvAnalyzer.cc
@@ -14,12 +14,38 @@
 #include "DataFormats/HepMCCandidate/interface/GenParticle.h"
 #include "DataFormats/HepMCCandidate/interface/GenParticleFwd.h"
 
+//ROOT headers
+#include "Math/QuantFuncMathCore.h"
+
 //STL headers 
 #include <vector>
 #include <memory>
 #include <cmath>
 #include <iostream>
 using std::vector;
+
+//some math functions
+namespace {
+	//ref: https://twiki.cern.ch/twiki/bin/viewauth/CMS/PoissonErrorBars
+	double PoissonErrorLow(int N){
+		const double alpha = 1 - 0.6827; //1 sigma interval
+		double L = (N==0) ? 0 : (ROOT::Math::gamma_quantile(alpha/2,N,1.));
+		return N - L;
+	}
+	double PoissonErrorUp(int N){
+		const double alpha = 1 - 0.6827; //1 sigma interval
+		double U = (ROOT::Math::gamma_quantile_c(alpha/2,N+1,1.));
+		return U - N;
+	}
+	//use PoissonErrorUp by default
+	double EffError(int pass, int total, bool up=true){
+		int fail = total-pass;
+		double eff = (double)pass/(double)total;
+		double err_p = up ? PoissonErrorUp(pass) : PoissonErrorLow(pass);
+		double err_f = up ? PoissonErrorUp(fail) : PoissonErrorLow(fail);
+		return sqrt(pow(1-eff,2)*pow(err_p,2) + pow(eff,2)*pow(err_f,2))/(double)total;
+	}
+}
 
 //
 // class declaration
@@ -82,8 +108,8 @@ void RinvAnalyzer::analyze(const edm::Event& iEvent, const edm::EventSetup& iSet
 
 void RinvAnalyzer::endJob()
 {
-	std::cout << "Pseudoscalar rinv = " << double(nPseudoscalarInv)/double(nPseudoscalar) << std::endl;
-	std::cout << "Vector rinv = " << double(nVectorInv)/double(nVector) << std::endl;
+	std::cout << "Pseudoscalar rinv = " << double(nPseudoscalarInv)/double(nPseudoscalar) << " +/- " << EffError(nPseudoscalarInv,nPseudoscalar) << std::endl;
+	std::cout << "Vector rinv = " << double(nVectorInv)/double(nVector) << " +/- " << EffError(nVectorInv,nVector)  << std::endl;
 }
 
 // ------------ method fills 'descriptions' with the allowed parameters for the module  ------------
