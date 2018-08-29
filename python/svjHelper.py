e2463087c060c33c2b3c9c7820da4510c9fef44b
Kevin Pedro <kpedro88@gmail.com>
2018-08-29 09:22:07 -0500

    use default ptminfsr setting

--- python/svjHelper.py
+++ python/svjHelper.py
@@ -202,7 +202,6 @@ class svjHelper(object):
             'HiddenValley:Lambda = {:g}'.format(self.lambdaHV),
             'HiddenValley:nFlav = {:d}'.format(self.n_f),
             'HiddenValley:probVector = 0.0',
-            'HiddenValley:pTminFSR = {:g}'.format(self.mSqua),
             # decouple
             '4900001:m0 = 5000',
             '4900002:m0 = 5000',
