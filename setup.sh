c953e57cd9706b7993476b760c103b8c5c4adad3
Kevin Pedro <kpedro88@gmail.com>
2018-09-25 10:11:02 -0500

    option to install pythia8.230 w/ ME correction

--- setup.sh
+++ setup.sh
@@ -12,10 +12,64 @@ usage() {
 	$ECHO "-c <RELEASE>  \tCMSSW release to install (e.g. CMSSW_7_1_26)"
 	$ECHO "-f [fork]     \tclone from specified fork (default = kpedro88)"
 	$ECHO "-b [branch]   \tclone specified branch (default = master)"
-	$ECHO "-p            \tinstall Pythia 8.226"
+	$ECHO "-p            \tinstall Pythia 8.230 w/ ME correction"
 	$ECHO "-a            \tinstall analysis code"
 	$ECHO "-h            \tprint this message and exit"
 	exit $1
+}
+
+# -------------------------------------------------------------------------------------
+# pythia installation
+# -------------------------------------------------------------------------------------
+
+installPythia() {
+	cd ${CMSSW_BASE}
+
+	# some really bad ways to get info out of scram
+	HEPMC_BASE=$(scram tool info hepmc | grep "HEPMC_BASE" | sed 's/HEPMC_BASE=//')
+	BOOST_BASE=$(scram tool info boost | grep "BOOST_BASE" | sed 's/BOOST_BASE=//')
+	LHAPDF_BASE=$(scram tool info lhapdf | grep "LHAPDF_BASE" | sed 's/LHAPDF_BASE=//')
+
+	# get pythia8 source and compile
+	git clone git@github.com:kpedro88/pythia8 -b "MECorr230"
+	cd pythia8
+	# configure for c++11 if 7_1_X
+	EXTRA=""
+	case $CMSSW_VERSION in
+	CMSSW_7_1_*)
+		EXTRA='--cxx-common="-std=c++11 -fPIC"'
+	;;
+	esac
+	./configure --enable-shared --with-boost=${BOOST_BASE} --with-hepmc2=${HEPMC_BASE} --with-lhapdf6=${LHAPDF_BASE} --with-lhapdf6-plugin=LHAPDF6.h "$EXTRA"
+	make -j 8
+	make install
+
+	# create xml for tool
+	cd $CMSSW_BASE
+	cat << 'EOF_TOOLFILE' > pythia8.xml
+<tool name="pythia8" version="230">
+  <lib name="pythia8"/>
+  <client>
+    <environment name="PYTHIA8_BASE" default="$CMSSW_BASE/pythia8"/>
+    <environment name="LIBDIR" default="$PYTHIA8_BASE/lib"/>
+    <environment name="INCLUDE" default="$PYTHIA8_BASE/include"/>
+  </client>
+  <runtime name="PYTHIA8DATA" value="$PYTHIA8_BASE/share/Pythia8/xmldoc"/>
+  <use name="hepmc"/>
+  <use name="lhapdf"/>
+</tool>
+EOF_TOOLFILE
+
+	# install tool in scram
+	cp ${CMSSW_BASE}/pythia8.xml ${CMSSW_BASE}/config/toolbox/${SCRAM_ARCH}/tools/selected
+	scram setup pythia8
+
+	# better to use 'scram b checkdeps' but this is not available in 71X
+	scram b echo_pythia8_USED_BY | tr ' ' '\n' | grep "self" | cut -d'/' -f2-3 | sort -u > pkgs.txt
+
+	# update CMSSW dependencies
+	cd $CMSSW_BASE/src
+	git cms-addpkg -f ../pkgs.txt
 }
 
 CUR_DIR=`pwd`
@@ -76,48 +130,6 @@ if [ -n "$WHICH_CMSSW" ]; then
 fi
 
 # -------------------------------------------------------------------------------------
-# pythia installation
-# -------------------------------------------------------------------------------------
-
-if [ -n "$INSTALL_PYTHIA" ]; then
-	if [ -n "$WHICH_CMSSW" ]; then
-		# create xml for tool
-		cat << 'EOF_TOOLFILE' > pythia8.xml
-<tool name="pythia8" version="226">
-  <lib name="pythia8"/>
-  <client>
-    <environment name="PYTHIA8_BASE" default="$CMSSW_BASE/pythia8226"/>
-    <environment name="LIBDIR" default="$PYTHIA8_BASE/lib"/>
-    <environment name="INCLUDE" default="$PYTHIA8_BASE/include"/>
-  </client>
-  <runtime name="PYTHIA8DATA" value="$PYTHIA8_BASE/share/Pythia8/xmldoc"/>
-  <use name="hepmc"/>
-  <use name="lhapdf"/>
-</tool>
-EOF_TOOLFILE
-	fi
-
-	# get pythia8 source and compile
-	wget -q http://home.thep.lu.se/~torbjorn/pythia8/pythia8226.tgz
-	tar -xzf pythia8226.tgz
-	export PYTHIA8DATA=$CUR_DIR/pythia8226/share/Pythia8/xmldoc
-	cd pythia8226/
-	gmake
-
-	if [ -n "$WHICH_CMSSW" ]; then
-		cd $CUR_DIR
-		# install tool in scram
-		cp ${CMSSW_BASE}/pythia8.xml ${CMSSW_BASE}/config/toolbox/${SCRAM_ARCH}/tools/selected
-		scram setup pythia8
-
-		$ECHO "you can now link to the pythia8 libraries through scram!"
-	fi
-
-	# cleanup
-	rm pythia8226.tgz
-fi
-
-# -------------------------------------------------------------------------------------
 # CMSSW compilation
 # -------------------------------------------------------------------------------------
 
@@ -133,13 +145,8 @@ if [ -n "$WHICH_CMSSW" ]; then
 
 	# get packages
 	if [ -n "$INSTALL_PYTHIA" ]; then
-		# need to recompile packages that depend on pythia8
-		git cms-addpkg GeneratorInterface/EvtGenInterface GeneratorInterface/PartonShowerVeto GeneratorInterface/Pythia8Interface
-		IFS='_' read -r -a ARRAY_CMSSW <<< "$WHICH_CMSSW"
-		if [ "${ARRAY_CMSSW[1]}" -eq "7" ] && [ "${ARRAY_CMSSW[2]}" -eq "1" ] && [ "${ARRAY_CMSSW[3]}" -lt "28" ]; then
-			$ECHO "Merging FixPythia7126"
-			git cms-merge-topic kpedro88:FixPythia7126
-		fi
+		installPythia
+		eval `scramv1 runtime -sh`
 	fi
 	git clone git@github.com:kpedro88/CondorProduction Condor/Production
 	git clone git@github.com:${FORK}/SVJProduction SVJ/Production -b ${BRANCH}
