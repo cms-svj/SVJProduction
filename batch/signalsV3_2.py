298bd3faa92b17763813fec7eebef0d1c5db4482
Kevin Pedro <kpedro88@gmail.com>
2018-12-21 15:55:21 -0600

    update 1D signal dicts

--- batch/signalsV3_2.py
+++ batch/signalsV3_2.py
@@ -1,8 +1,8 @@
 from collections import OrderedDict
 
 params = OrderedDict([
-("mZprime", (3000,[1000, 1500, 2000, 2500, 3000, 3500, 4000])),
-("mDark", (20,[5, 20, 50])),
+("mZprime", (3000,range(500,4600,100))),
+("mDark", (20,[1]+range(10,110,10))),
 ("rinv", (0.3,[0.0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0])),
 ("alpha", ("peak",["peak", "high", "low"])),
 ])
@@ -23,8 +23,9 @@ for i, (p, vals) in enumerate(params.iteritems()):
         tmp = tuple(tmp)
         if tmp!=stmp: sigs.add(tmp)
 
-#print '\n'.join([str(s) for s in sigs])
-#print len(sigs)
+from signalsV3_1 import sigs as sigs1
+sigs = sigs - sigs1
+print len(sigs)
 
 flist = [OrderedDict([("mZprime", x[0]), ("mDark", x[1]), ("rinv", x[2]), ("alpha", x[3])]) for x in sorted(sigs)]
 
