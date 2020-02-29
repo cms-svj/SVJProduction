95a311b563c56fe63314fb331f556f87273473bf
Kevin Pedro <kpedro88@gmail.com>
2020-02-29 14:50:29 -0600

    avoid unicode characters

--- test/run_svj_tarball.sh
+++ test/run_svj_tarball.sh
@@ -3,6 +3,7 @@
 $CMSSW_RELEASE_BASE/src/GeneratorInterface/LHEInterface/data/run_generic_tarball_cvmfs.sh "$@"
 
 # convert madgraph ids to pythia ids
+# (also convert a unicode character)
 PDGIDS=(
 5000521,4900101 \
 49001010,4900101 \
@@ -34,10 +35,11 @@ PDGIDS=(
 9000026,4900006 \
 9000027,4900006 \
 9000028,4900006 \
+'\\x7',a \
 )
 
 for PDGID in ${PDGIDS[@]}; do
 	IDS=()
 	IFS="," read -a IDS <<< "$PDGID"
-	sed -i 's/'${IDS[0]}'/'${IDS[1]}'/g' cmsgrid_final.lhe
+	sed -i 's/'"${IDS[0]}"'/'"${IDS[1]}"'/g' cmsgrid_final.lhe
 done
