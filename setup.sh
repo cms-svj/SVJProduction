46b56c138f29d51a4ec4a18ddec524d452343c77
Kevin Pedro <kpedro88@gmail.com>
2022-01-12 14:54:29 -0600

    update setup script for UL

--- setup.sh
+++ setup.sh
@@ -5,84 +5,29 @@ case `uname` in
 	*) ECHO="echo" ;;
 esac
 
+# defaults
 ACCESS=https
+WHICH_CMSSW=CMSSW_10_6_29_patch1
+FORK=cms-svj
+BRANCH=Run2_UL
+CORES=8
 
 usage() {
 	$ECHO "setup.sh [options]"
 	$ECHO
 	$ECHO "Options:"
-	$ECHO "-c <RELEASE>  \tCMSSW release to install (e.g. CMSSW_7_1_26)"
-	$ECHO "-f [fork]     \tclone from specified fork (default = cms-svj)"
-	$ECHO "-b [branch]   \tclone specified branch (default = master)"
-	$ECHO "-p            \tinstall Pythia 8.230 w/ ME correction"
-	$ECHO "-a            \tinstall analysis code"
+	$ECHO "-c <RELEASE>  \tCMSSW release to install (default = $WHICH_CMSSW)"
+	$ECHO "-f [fork]     \tclone from specified fork (default = $FORK)"
+	$ECHO "-b [branch]   \tclone specified branch (default = $BRANCH)"
 	$ECHO "-s            \tuse protocol to clone (default = ${ACCESS}, alternative = ssh)"
+	$ECHO "-j [cores]    \t# cores for CMSSW compilation (default = ${CORES})"
 	$ECHO "-h            \tprint this message and exit"
 	exit $1
 }
 
-# -------------------------------------------------------------------------------------
-# pythia installation
-# -------------------------------------------------------------------------------------
-
-installPythia() {
-	cd ${CMSSW_BASE}
-
-	# some really bad ways to get info out of scram
-	HEPMC_BASE=$(scram tool info hepmc | grep "HEPMC_BASE" | sed 's/HEPMC_BASE=//')
-	BOOST_BASE=$(scram tool info boost | grep "BOOST_BASE" | sed 's/BOOST_BASE=//')
-	LHAPDF_BASE=$(scram tool info lhapdf | grep "LHAPDF_BASE" | sed 's/LHAPDF_BASE=//')
-
-	# get pythia8 source and compile
-	git clone git@github.com:kpedro88/pythia8 -b "MECorr230"
-	cd pythia8
-	# configure for c++11 if 7_1_X
-	EXTRA=""
-	case $CMSSW_VERSION in
-	CMSSW_7_1_*)
-		EXTRA='--cxx-common="-std=c++11 -fPIC"'
-	;;
-	esac
-	./configure --enable-shared --with-boost=${BOOST_BASE} --with-hepmc2=${HEPMC_BASE} --with-lhapdf6=${LHAPDF_BASE} --with-lhapdf6-plugin=LHAPDF6.h "$EXTRA"
-	make -j 8
-	make install
-
-	# create xml for tool
-	cd $CMSSW_BASE
-	cat << 'EOF_TOOLFILE' > pythia8.xml
-<tool name="pythia8" version="230">
-  <lib name="pythia8"/>
-  <client>
-    <environment name="PYTHIA8_BASE" default="$CMSSW_BASE/pythia8"/>
-    <environment name="LIBDIR" default="$PYTHIA8_BASE/lib"/>
-    <environment name="INCLUDE" default="$PYTHIA8_BASE/include"/>
-  </client>
-  <runtime name="PYTHIA8DATA" value="$PYTHIA8_BASE/share/Pythia8/xmldoc"/>
-  <use name="hepmc"/>
-  <use name="lhapdf"/>
-</tool>
-EOF_TOOLFILE
-
-	# install tool in scram
-	cp ${CMSSW_BASE}/pythia8.xml ${CMSSW_BASE}/config/toolbox/${SCRAM_ARCH}/tools/selected
-	scram setup pythia8
-
-	# better to use 'scram b checkdeps' but this is not available in 71X
-	scram b echo_pythia8_USED_BY | tr ' ' '\n' | grep "self" | cut -d'/' -f2-3 | sort -u > pkgs.txt
-
-	# update CMSSW dependencies
-	cd $CMSSW_BASE/src
-	git cms-addpkg -f ../pkgs.txt
-}
-
 CUR_DIR=`pwd`
-WHICH_CMSSW=""
-FORK=cms-svj
-BRANCH=master
-INSTALL_PYTHIA=""
-INSTALL_ANALYSIS=""
 #check arguments
-while getopts "c:f:b:s:pah" opt; do
+while getopts "c:f:b:s:j:h" opt; do
 	case "$opt" in
 	c) WHICH_CMSSW=$OPTARG
 	;;
@@ -92,18 +37,12 @@ while getopts "c:f:b:s:pah" opt; do
 	;;
 	s) ACCESS=$OPTARG
 	;;
-	p) INSTALL_PYTHIA=yes
-	;;
-	a) INSTALL_ANALYSIS=yes
+	j) CORES=$OPTARG
 	;;
 	h) usage 0
 	;;
 	esac
 done
-
-if [ -z "$WHICH_CMSSW" ] && [ -z "$INSTALL_PYTHIA" ]; then
-	usage 1
-fi
 
 if [ "$ACCESS" = "ssh" ]; then
 	export ACCESS_GITHUB=git@github.com:
@@ -116,48 +55,29 @@ else
 fi
 
 # OS check: try redhat-release first to handle Singularity case
+# kept in view of handling post-SL7 OS
 if [[ -f "/etc/redhat-release" ]]; then
 	VERSION_TMP=`awk -F'[ .]' '{print $4}' "/etc/redhat-release"`
-	POSSIBLE_VERSIONS=( 6 7 )
-	if [[ "${POSSIBLE_VERSIONS[@]} " =~ " ${VERSION_TMP}" ]]; then
+	POSSIBLE_VERSIONS=( 7 )
+	if [[ "${POSSIBLE_VERSIONS[@]} " =~ "${VERSION_TMP}" ]]; then
 		SLC_VERSION="slc${VERSION_TMP}"
 	else
-		echo "WARNING::Unknown SLC version. Defaulting to SLC6."
-		SLC_VERSION="slc6"
+		echo "WARNING::Unknown SLC version. Defaulting to SLC7."
+		SLC_VERSION="slc7"
 	fi
-elif [[ `uname -r` == *"el6"* ]]; then
-	SLC_VERSION="slc6"
 elif [[ `uname -r` == *"el7"* ]]; then
 	SLC_VERSION="slc7"
 else
-	echo "WARNING::Unknown SLC version. Defaulting to SLC6."
-SLC_VERSION="slc6"
+	echo "WARNING::Unknown SLC version. Defaulting to SLC7."
+SLC_VERSION="slc7"
 fi
 
 # -------------------------------------------------------------------------------------
 # CMSSW release area
 # -------------------------------------------------------------------------------------
-CMSSW71X=""
 if [ -n "$WHICH_CMSSW" ]; then
 	case $WHICH_CMSSW in
-	CMSSW_7_1_*)
-		if [[ $SLC_VERSION == slc7 ]]; then
-			echo "CMSSW_7_1_X not available on slc7"
-			exit 1
-		fi
-		export SCRAM_ARCH=slc6_amd64_gcc481
-		CMSSW71X=true
-	;;
-	CMSSW_8_0_*)
-		export SCRAM_ARCH=${SLC_VERSION}_amd64_gcc530
-	;;
-	CMSSW_9_3_*)
-		export SCRAM_ARCH=${SLC_VERSION}_amd64_gcc630
-	;;
-	CMSSW_9_4_*)
-		export SCRAM_ARCH=${SLC_VERSION}_amd64_gcc630
-	;;
-	CMSSW_10_2_*)
+	CMSSW_10_6_*)
 		export SCRAM_ARCH=${SLC_VERSION}_amd64_gcc700
 	;;
 	*)
@@ -181,26 +101,12 @@ if [ -n "$WHICH_CMSSW" ]; then
 	eval `scramv1 runtime -sh`
 	cd src
 	git cms-init $ACCESS_CMSSW
-	if [[ $WHICH_CMSSW = CMSSW_9_4_* ]]; then
-		git cms-merge-topic -u $ACCESS_CMSSW kpedro88:debugEventSetupMultithreaded9410
-	elif [[ $WHICH_CMSSW = CMSSW_10_2_* ]]; then
-		git cms-merge-topic -u $ACCESS_CMSSW kpedro88:suep102X
-	fi
 
-	if [ -n "$INSTALL_ANALYSIS" ]; then
-		git clone ${ACCESS_GITHUB}kpedro88/Analysis -b SVJ2017-gen
-	fi
-
-	# get packages
-	if [ -n "$INSTALL_PYTHIA" ]; then
-		installPythia
-		eval `scramv1 runtime -sh`
-	fi
 	git clone ${ACCESS_GITHUB}kpedro88/CondorProduction Condor/Production
 	git clone ${ACCESS_GITHUB}${FORK}/SVJProduction SVJ/Production -b ${BRANCH}
 
 	# use as little of genproductions as possible
-	git clone --depth 1 --no-checkout ${ACCESS_GITHUB}kpedro88/genproductions -b run2 Configuration/GenProduction
+	git clone --depth 1 --no-checkout ${ACCESS_GITHUB}cms-svj/genproductions -b Run2_UL Configuration/GenProduction
 	# setup sparse checkout
 	cd Configuration/GenProduction
 	git config core.sparsecheckout true
@@ -213,29 +119,9 @@ if [ -n "$WHICH_CMSSW" ]; then
 	git read-tree -mu HEAD
 	cd $CMSSW_BASE/src
 
-	# setup preprocessor flag for old CMSSW version
-	cd SVJ/Production/interface
-	if [ -n "$CMSSW71X" ]; then
-		cat << EOF > common.h
-#ifndef SVJ_Production_common_h
-#define SVJ_Production_common_h
-
-#define CMSSW71X
-
-#endif
-EOF
-	else
-		cat << EOF > common.h
-#ifndef SVJ_Production_common_h
-#define SVJ_Production_common_h
-
-#endif
-EOF
-	fi
-	cd $CMSSW_BASE/src
-
-	scram b -j 8
+	scram b -j $CORES
 	cd SVJ/Production/batch
 	python $CMSSW_BASE/src/Condor/Production/python/linkScripts.py
+	python $CMSSW_BASE/src/Condor/Production/python/cacheAll.py
 	ln -s $CMSSW_BASE/src/Condor/Production/python/manageJobs.py .
 fi
