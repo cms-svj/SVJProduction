97d35a52d4a11e0fc8d23c4afdeb2f738b3ab364
Kevin Pedro <kpedro88@gmail.com>
2022-01-20 12:41:00 -0600

    improve examples

---
+++ batch/signals_MG_ex.py
@@ -0,0 +1,4 @@
+flist = [
+    {"channel": "s", "mMediator": 3000, "mDark": 20, "rinv": 0.3, "alpha": "peak", "boost": 375},
+    {"channel": "t", "mMediator": 3000, "mDark": 20, "rinv": 0.3, "alpha": "peak", "yukawa": 1},
+]
---
+++ batch/signals_P8_ex.py
@@ -0,0 +1,3 @@
+flist = [
+    {"channel": "s", "mMediator": 3000, "mDark": 20, "rinv": 0.3, "alpha": "peak"},
+]
