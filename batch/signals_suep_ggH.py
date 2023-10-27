cb83c08703841299397b5fe7c96369dd02917cc2
Kevin Pedro <kpedro88@gmail.com>
2023-10-27 04:33:10 -0500

    suep test signals

---
+++ batch/signals_suep_ggH.py
@@ -0,0 +1,22 @@
+# signal dictionary used for SUEP anomaly trigger test
+
+flist = [
+    {
+        "mMediator": 125,
+        "mDark": 2.0,
+        "temperature": 2.0,
+        "decay": "generic",
+    },
+    {
+        "mMediator": 125,
+        "mDark": 2.0,
+        "temperature": 2.0,
+        "decay": "darkPho",
+    },
+    {
+        "mMediator": 125,
+        "mDark": 2.0,
+        "temperature": 2.0,
+        "decay": "darkPhoHad",
+    },
+]
