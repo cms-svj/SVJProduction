241c7ab22c29e2c8de012038c7285ba3af89884d
Kevin Pedro <kpedro88@gmail.com>
2023-11-09 09:45:55 -0600

    update boosted scan

--- python/generateScanBoosted.py
+++ python/generateScanBoosted.py
@@ -6,6 +6,7 @@ from collections import OrderedDict
 from copy import deepcopy
 from argparse import ArgumentParser, ArgumentDefaultsHelpFormatter
 import numpy as np
+import os, json
 
 # implementation of recursive loop over any number of dimensions
 # creates grid of all possible combinations of parameter values
@@ -21,7 +22,8 @@ def varyAll(pos,paramlist,sig,sigs):
             varyAll(pos+1,paramlist,stmp,sigs)
 
 parser = ArgumentParser(formatter_class=ArgumentDefaultsHelpFormatter)
-parser.add_argument("-n","--num", dest="num", type=int, default=20000, help="number of events for model point w/ weight 1.0 (before filter)")
+parser.add_argument("-n","--num", dest="num", type=int, default=1000, help="number of events per job for model point w/ weight 1.0 (before filter)")
+parser.add_argument("-j","--jobs", dest="jobs", type=int, default=20, help="number of jobs")
 parser.add_argument("-a","--acc", dest="acc", type=float, default=0.0, help="increase number of events based on acceptance up to this maximum factor")
 args = parser.parse_args()
 
@@ -40,9 +42,9 @@ params = OrderedDict([
 
 # acceptance values vs. each param
 acc = OrderedDict([
-    ("mDark", ([1,5,10],[0.13,0.12,0.11])),
-    ("mZprime", ([200,250,300,350,400,450,500,550],[0.079,0.088,0.098,0.11,0.11,0.12,0.13,0.14])),
-    ("rinv", ([0.1,0.3,0.7],[0.044,0.11,0.12])),
+    ("mDark", ([1,5,10],[0.12,0.12,0.11])),
+    ("mZprime", ([200,250,300,350,400,450,500,550],[0.081,0.089,0.099,0.11,0.12,0.12,0.13,0.14])),
+    ("rinv", ([0,0.05,0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9,1],[0.017,0.024,0.045,0.082,0.11,0.12,0.13,0.13,0.12,0.11,0.081,0.058])),
 ])
 # acceptance w/ benchmark param values
 base_acc = 0.11
@@ -99,6 +101,7 @@ points = []
 numevents_before = 0
 numevents_after = 0
 base_filter_eff = 0.5
+flist = []
 for point in sorted(sigs):
     mZprime = point[0]
     mDark = point[1]
@@ -119,67 +122,15 @@ for point in sorted(sigs):
         max_weight = weight*args.acc
         weight = np.clip(weight/this_acc,min_weight,max_weight)
 
-    helper.setModel("s",mZprime,mDark,rinv,alpha)
-    
-    pdict = {
-        'weight': weight,
-        'processParameters': helper.getPythiaSettings(),
-        'name': helper.getOutName(outpre="SVJ"),
-    }
-    points.append(pdict)
-    numevents_before += args.num*weight
-    numevents_after += args.num*weight*filter_eff
+    maxEvents = int(args.num*weight)
+    flist.append(OrderedDict([("channel", "s"), ("mMediator", mZprime), ("mDark", mDark), ("rinv", rinv), ("alpha", alpha), ("maxEvents", maxEvents)]))
+
+    numevents_before += maxEvents*args.jobs
+    numevents_after += maxEvents*args.jobs*filter_eff
 
 # some info on the scan
 print("This scan will contain "+str(len(sigs))+" model points, "+str(int(numevents_before))+" events before filter, "+str(int(numevents_after))+" events after filter")
 
-# format last part of config (loop over all points)
-# todo: get gridpack path
-last_part = """
-for point in points:
-    basePythiaParameters = cms.PSet(
-        pythia8CommonSettingsBlock, 
-        {0},
-        pythia8PSweightsSettingsBlock,
-        processParameters = cms.vstring(point['processParameters']),
-        JetMatchingParameters = cms.vstring(),
-        parameterSets = cms.vstring(
-            'pythia8CommonSettings',
-            '{1}',
-            'pythia8PSweightsSettings',
-            'processParameters',
-        )
-    )
-
-    generator.RandomizedParameters.append(
-        cms.PSet(
-            GridpackPath = cms.string(''),
-            ConfigWeight = cms.double(point['weight']),
-            ConfigDescription = cms.string(point['name']),
-            PythiaParameters = basePythiaParameters,
-        ),
-    )
-
-darkhadronZ2filter = cms.EDFilter("MCParticleModuloFilter",
-    moduleLabel = {2},
-    particleIDs = cms.vint32(51,53),
-    multipleOf = cms.uint32(4),
-    absID = cms.bool(True),
-)
-
-darkquarkFilter = cms.EDFilter("MCParticleModuloFilter",
-    moduleLabel = {2},
-    particleIDs = cms.vint32(4900101),
-    multipleOf = cms.uint32(2),
-    absID = cms.bool(True),
-    min = cms.uint32(2),
-    status = cms.int32(23),
-)
-
-ProductionFilterSequence = cms.Sequence(generator+darkhadronZ2filter+darkquarkFilter)
-""".format(tune_block,tune_block.replace("Block",""),gen_tag)
-
-with open("SVJ_Boosted_Scan_"+tune_suff+"_cff.py",'w') as ofile:
-    ofile.write(first_part)
-    ofile.write("\npoints = "+str(points)+"\n")
-    ofile.write(last_part)
+with open(os.path.expandvars("$CMSSW_BASE/src/SVJ/Production/batch/signals_boosted_scan.py"),'w') as ofile:
+    lines = ["flist = ["] + ['    '+json.dumps(d)+',' for d in flist] + ["]"]
+    ofile.write('\n'.join(lines))
