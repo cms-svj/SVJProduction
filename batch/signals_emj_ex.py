d71857812a2fbb372bf025649702cf10656e808b
Kevin Pedro <kpedro88@gmail.com>
2024-02-01 11:05:48 -0600

    add some EMJ examples

---
+++ batch/signals_emj_ex.py
@@ -0,0 +1,4 @@
+flist = [
+    {"mMediator": 1000, "mDark": 20, "kappa": 1, "mode": "aligned", "type": "down"},
+    {"mMediator": 1000, "mDark": 20, "kappa": 100, "mode": "unflavored", "type": "down"},
+]
