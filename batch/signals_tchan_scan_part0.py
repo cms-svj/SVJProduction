a982d4c9a89f40dd068ee83f226b192be738f4c1
Kevin Pedro <kpedro88@gmail.com>
2024-10-09 09:26:29 -0500

    manually split some signals into part0 as a test

---
+++ batch/signals_tchan_scan_part0.py
@@ -0,0 +1,14 @@
+flist = [
+    {"channel": "t", "mMediator": 500, "mDark": 20, "rinv": 0.3, "alpha": "peak", "yukawa": 1.0, "maxEvents": 4000},
+    {"channel": "t", "mMediator": 600, "mDark": 20, "rinv": 0.3, "alpha": "peak", "yukawa": 1.0, "maxEvents": 4000},
+    {"channel": "t", "mMediator": 700, "mDark": 20, "rinv": 0.3, "alpha": "peak", "yukawa": 1.0, "maxEvents": 4000},
+    {"channel": "t", "mMediator": 800, "mDark": 20, "rinv": 0.3, "alpha": "peak", "yukawa": 1.0, "maxEvents": 2267},
+    {"channel": "t", "mMediator": 900, "mDark": 20, "rinv": 0.3, "alpha": "peak", "yukawa": 1.0, "maxEvents": 2267},
+    {"channel": "t", "mMediator": 1000, "mDark": 20, "rinv": 0.3, "alpha": "peak", "yukawa": 1.0, "maxEvents": 2000},
+    {"channel": "t", "mMediator": 1500, "mDark": 20, "rinv": 0.3, "alpha": "peak", "yukawa": 1.0, "maxEvents": 2000},
+    {"channel": "t", "mMediator": 2000, "mDark": 20, "rinv": 0.3, "alpha": "peak", "yukawa": 1.0, "maxEvents": 2000},
+    {"channel": "t", "mMediator": 2500, "mDark": 20, "rinv": 0.3, "alpha": "peak", "yukawa": 1.0, "maxEvents": 2000},
+    {"channel": "t", "mMediator": 3000, "mDark": 20, "rinv": 0.3, "alpha": "peak", "yukawa": 1.0, "maxEvents": 2632},
+    {"channel": "t", "mMediator": 3500, "mDark": 20, "rinv": 0.3, "alpha": "peak", "yukawa": 1.0, "maxEvents": 2632},
+    {"channel": "t", "mMediator": 4000, "mDark": 20, "rinv": 0.3, "alpha": "peak", "yukawa": 1.0, "maxEvents": 2664},
+]
